52154ea35008b1237b7e54719b254f92a1c7feaa
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-06 11:56:06 -0800

    Completed exercise 03

--- src/ex03.ts
+++ src/ex03.ts
@@ -3,10 +3,10 @@
 // If two numbers are passed, return their sum.
 // If two strings are passed, return their concatenated (combined) value.
 
-function combine(a, b);
-function combine(a, b);
-function combine(a, b) {
-  
+function combine(a: number, b: number): number;
+function combine(a: string, b: string): number;
+function combine(a: any, b: any): any {
+  return a + b
 }
 
 console.log(combine(10, 20));      // Expected output: 30
